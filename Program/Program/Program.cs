45a583b8ace5a6bdcdfb7ed7c389c6f1d9ddbcb3
irinainikitina <irisha511314@gmail.com>
2024-04-02 01:42:54 +0300

    Created program

---
+++ Program/Program/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(',');
+
+        string[] resultArray = FilterArray(inputArray);
+
+        Console.WriteLine("Результат:");
+        foreach (string item in resultArray)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    static string[] FilterArray(string[] inputArray)
+    {
+        int count = 0;
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+        int index = 0;
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                resultArray[index] = item;
+                index++;
+            }
+        }
+
+        return resultArray;
+    }
+}
